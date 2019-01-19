bddd4535aa47688892b68702fd27b42b8ee7ad40
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2019-01-19 23:14:07 +0800

    initial update

---
+++ conf.js
@@ -0,0 +1,25 @@
+/*jslint node: true */
+"use strict";
+
+exports.port = null;
+//exports.myUrl = 'wss://mydomain.com/bb';
+exports.bServeAsHub = false;
+exports.bLight = false;
+
+exports.storage = 'sqlite';
+
+
+exports.hub = 'hub.worldnuqumoritytransporters.com';
+exports.deviceName = 'Witness';
+exports.permanent_pairing_secret = 'randomstring';
+exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
+exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
+
+exports.bSingleAddress = true;
+exports.THRESHOLD_DISTANCE = 50;
+exports.MIN_INTERVAL = 60*1000; // min interval between witnessings in milliseconds
+exports.MIN_AVAILABLE_WITNESSINGS = 100;
+
+exports.KEYS_FILENAME = 'keys.json';
+
+console.log('finished witness conf');
